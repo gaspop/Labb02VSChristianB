271b17167738d13fc416f8c31ef47fd06fa36910
Christian Blomqvist <chrille.blomqvist@gmail.com>
2017-01-18 21:11:00 +0100

    Initial commit

---
+++ Labb02/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Labb02
+{
+    [Activity(Label = "@string/ApplicationName", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        TextView tv;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            tv = FindViewById<TextView>(Resource.Id.tv1);
+            tv.Text = "Hej hej!";
+
+        }
+    }
+}
+
