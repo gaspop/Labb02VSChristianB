835e5b983ebb3a4c6d62a593be211cd79b61d168
gaspop <chrille.blomqvist@gmail.com>
2017-01-20 16:49:34 +0100

    Added Main Menu elements.

---
+++ Labb02/EventActivity.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Labb02
+{
+	[Activity(Label = "EventActivity")]
+	public class EventActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.Event);
+			// Create your application here
+		}
+	}
+}
--- Labb02/MainActivity.cs
+++ Labb02/MainActivity.cs
@@ -1,14 +1,17 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
 
 namespace Labb02
 {
     [Activity(Label = "@string/ApplicationName", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        TextView tv;
-        Button btn;
+        //TextView tv;
+        Button btnNewEvent;
+		//Button btnShowAllEvents;
+		//Button btnCreateReports;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -16,16 +19,32 @@ namespace Labb02
 
             // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.Main);
+			setupButtons();
+            //tv = FindViewById<TextView>(Resource.Id.tv1);
+            //btn = FindViewById<Button>(Resource.Id.btn1);
 
-            tv = FindViewById<TextView>(Resource.Id.tv1);
-            btn = FindViewById<Button>(Resource.Id.btn1);
-
+			/*
             btn.Click += delegate
             {
                 tv.Text = "Hej hej!";
             };
+            */
             
         }
+
+		private void setupButtons()
+		{
+			btnNewEvent = FindViewById<Button>(Resource.Id.btnNewEvent);
+			//btnShowAllEvents = FindViewById<Button>(Resource.Id.btnShowAllEvents);
+			//btnCreateReports = FindViewById<Button>(Resource.Id.btnCreateReports);
+
+			btnNewEvent.Click += delegate
+			{
+				Intent i = new Intent(this, typeof(EventActivity));
+				StartActivity(i);
+			};
+		}
+
     }
 }
 
