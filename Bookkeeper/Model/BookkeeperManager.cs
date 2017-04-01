924a2624dc29f922bab75c4129385c14519b51e1
Christian Blomqvist <chrille.blomqvist@gmail.com>
2017-04-01 16:22:21 +0200

    Fixed formatting error.

--- Bookkeeper/Model/BookkeeperManager.cs
+++ Bookkeeper/Model/BookkeeperManager.cs
@@ -1,55 +1,104 @@
-using System; using System.Collections.Generic; using System.Linq; using System.Text;  using Android.App; using Android.Content; using Android.Runtime; using Android.OS; using Android.Views; using Android.Widget; using Android.Util;  using SQLite;  namespace Bookkeeper.Model {     public class BookkeeperManager     { 		public static readonly string dbPath = System.Environment.GetFolderPath 		                                             (System.Environment.SpecialFolder.Personal) 		                                             + @"database.db"; 		         private static BookkeeperManager instance;          public static BookkeeperManager Instance         {             get             {                 if (instance == null)                 {                     instance = new BookkeeperManager();                 }                 return instance;             }         }  		private SQLiteConnection db;          private BookkeeperManager()         { 			db = new SQLiteConnection(dbPath); 			CreateTableData(); 		}  		private void CreateTableData() 		{ 			db.CreateTable<Entry>(); 			db.CreateTable<Account>();  			if (db.Table<Account>().Count() == 0) 			{ 				db.Insert(new Account() { Number = 3000, Name = "Försäljning", Type = AccountType.Income }); 				db.Insert(new Account() { Number = 3040, Name = "Tjänster", Type = AccountType.Income }); 				db.Insert(new Account() { Number = 3911, Name = "Hyresintäkter", Type = AccountType.Income });  				db.Insert(new Account() { Number = 4010, Name = "Varuinköp", Type = AccountType.Expense }); 				db.Insert(new Account() { Number = 5810, Name = "Biljetter", Type = AccountType.Expense }); 				db.Insert(new Account() { Number = 5830, Name = "Kost och logi", Type = AccountType.Expense }); 				db.Insert(new Account() { Number = 6110, Name = "Kontorsmaterial", Type = AccountType.Expense });  				db.Insert(new Account() { Number = 1910, Name = "Kassa", Type = AccountType.Money }); 				db.Insert(new Account() { Number = 1920, Name = "Bank", Type = AccountType.Money }); 				db.Insert(new Account() { Number = 2010, Name = "Eget kapital", Type = AccountType.Money }); 			}  			db.CreateTable<TaxRate>(); 			if (db.Table<TaxRate>().Count() == 0) 			{ 				db.Insert(new TaxRate() { Rate = 0.25f }); 				db.Insert(new TaxRate() { Rate = 0.12f }); 				db.Insert(new TaxRate() { Rate = 0.06f }); 			}  			/* Genererar slump 			if (db.Table<Entry>().Count() == 0) 			    GenerateEntries(); 			*/  		}          public List<Entry> Entries         {             get { 				return db.Table<Entry>().ToList();
-			}         } 		public List<Account> Accounts 		{ 			get 			{ 				return db.Table<Account>().ToList(); 			} 		}          public List<Account> IncomeAccounts         {             get  			{
-				return db.Table<Account>().Where(a => a.Type == AccountType.Income).ToList();  			}         }          public List<Account> ExpenseAccounts         {             get  			{ 				return db.Table<Account>().Where(a => a.Type == AccountType.Expense).ToList();
-			}         }          public List<Account> MoneyAccounts         {             get  			{ 				return db.Table<Account>().Where(a => a.Type == AccountType.Money).ToList();
-			}         }          public List<TaxRate> TaxRates         {             get  			{ 				return db.Table<TaxRate>().ToList();
-			}         }
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-		public void AddEntry(Entry entry)
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Graphics;
+
+namespace Bookkeeper.Model
+{
+	class AccountAdapter : BaseAdapter<Account>
+	{
+		BookkeeperManager manager;
+		Activity activity;
+		List<Account> list;
+
+		public AccountAdapter(Activity activity)
 		{
-			db.Insert(new Entry
-			{ 				Type = entry.Type, 				Date = entry.Date, 				Description = entry.Description, 				TypeAccountNumber = entry.TypeAccountNumber, 				MoneyAccountNumber = entry.MoneyAccountNumber, 				TotalSum = entry.TotalSum, 				Rate = entry.Rate
-			});         }  		public void UpdateEntry(Entry entry) 		{ 			db.Update(entry); 		}  		public string GetTaxReport() 		{ 			var result = Entries.OrderBy(e => e.Date).Select(e =>
-							String.Format("{0}\n{1}\n{2}",  	                              e.Date.ToString("yyyy-MM-dd"),  	                              e.Description, 			                              (e.TotalSum - Math.Round(e.TotalSum / (1.0 + e.Rate))) 			                              * ((e.Type == EntryType.Income) ? 1 : (-1) ))); 			 			return string.Join("\n\n", result); 		}
+			manager = BookkeeperManager.Instance;
+			this.activity = activity;
+			list = manager.Accounts.OrderBy(a => a.Type).ToList();
+		}
 
+		public override Account this[int position]
+		{
+			get { return list[position]; }
+		}
 
-        /* Hjälpfunktion som genererar händelser för test.
-        private void GenerateEntries()
-        {
-            List<Account> accIn = IncomeAccounts;
-            List<Account> accEx = ExpenseAccounts;
-            List<Account> accMoney = MoneyAccounts;
-            List<TaxRate> VATs = TaxRates;
+		public override int Count
+		{
+			get
+			{
+				return list.Count;
+			}
+		}
 
-            string[] eventText = {  "Glass.",
-                                    "Öl.",
-                                    "Sprit.",
-                                    "Kalle Anka-tidning.",
-                                    "Böcker.",
-                                    "Hyra.",
-                                    "Ramlade på fyllan.",
-                                    "Flytthjälp.",
-                                    "Halkade på ett bananskal.",
-                                    "\"Hjälpte\" en \"vän\" nedför en trappa." };
+		public override long GetItemId(int position)
+		{
+			return position;
+		}
 
-            Random rnd = new Random();
-            DateTime start = new DateTime(2016, 1, 1);
-            for (int i = 0; i < 40; i++)
-            {
-                int range = (DateTime.Today - start).Days;
-                EntryType t = rnd.Next(2) == 0 ? EntryType.Income : EntryType.Expense;
-                Entry e = new Entry();
-                e.Type = t;
-                e.Date = start.AddDays(rnd.Next(range));
-                e.Description = eventText[rnd.Next(eventText.Length)];
-                if (t == EntryType.Income)
-                    e.TypeAccountNumber = accIn[rnd.Next(accIn.Count)].Number;
-                else
-                    e.TypeAccountNumber = accEx[rnd.Next(accEx.Count)].Number;
-                e.MoneyAccountNumber = accMoney[rnd.Next(accMoney.Count)].Number;
-                e.SumTotal = 25 + (rnd.Next(25) * 25) + (rnd.Next(2) * (rnd.Next(25) * 250));
-                e.Rate = VATs[rnd.Next(VATs.Count)].Rate;
-                AddEntry(e);
-            }
-        }*/ 
-    } }
+		public override View GetView(int position, View convertView, ViewGroup parent)
+		{
+			View view = convertView ?? activity.LayoutInflater.Inflate(Resource.Layout.AccountReportItem, parent, false);
+
+			TextView tvName = view.FindViewById<TextView>(Resource.Id.reportAccount);
+			TableLayout tlEntries = view.FindViewById<TableLayout>(Resource.Id.reportTableEntries);
+			LinearLayout llResult = view.FindViewById<LinearLayout>(Resource.Id.reportResult);
+			TextView tvSum = view.FindViewById<TextView>(Resource.Id.reportTotalSum);
+
+			Account account = list[position];
+			List<Entry> entries = manager.Entries.OrderBy(e => e.Date)
+						.Where(e => e.TypeAccountNumber == account.Number
+	             	             || e.MoneyAccountNumber == account.Number).ToList();
+
+			tvName.Text = account.Name + " (" + account.Number + ")";
+			tlEntries.RemoveAllViews();
+			if (entries.Count > 0)
+			{
+				int TotalSum = 0;
+				for (int i = 0; i < entries.Count; i++)
+				{
+					entries[i].TotalSum *= (entries[i].Type == EntryType.Income) ? 1 : (-1);
+					TotalSum += entries[i].TotalSum;
+					tlEntries.AddView(NewTableRow(entries[i]));
+				}
+				tvSum.Text = TotalSum + " kr";
+				tlEntries.Visibility = ViewStates.Visible;
+				llResult.Visibility = ViewStates.Visible;
+			}
+			else
+			{
+				tlEntries.Visibility = ViewStates.Gone;
+				llResult.Visibility = ViewStates.Gone;
+			}
+
+			view.SetBackgroundColor
+			  (position % 2 == 0 ? new Color(56, 56, 56) : view.DrawingCacheBackgroundColor);
+			return view;
+				
+		}
+
+		private TableRow NewTableRow(Entry e)
+		{
+			TableRow tr = (TableRow)View.Inflate(activity, Resource.Layout.ReportTableRow, null);
+			TextView tvRowDate = tr.FindViewById<TextView>(Resource.Id.rowDate);
+			TextView tvRowDescription = tr.FindViewById<TextView>(Resource.Id.rowDescription);
+			TextView tvRowSum = tr.FindViewById<TextView>(Resource.Id.rowSum);
+
+			tvRowDate.Text = e.Date.ToString("yyyy-MM-dd");
+			tvRowDescription.Text = e.Description;
+			tvRowSum.Text = string.Format("{0}", e.TotalSum);
+			tvRowSum.SetTextColor
+			        (e.Type == EntryType.Income ? Color.ForestGreen : Color.OrangeRed);
+
+			return tr;
+		}
+	}
+}
